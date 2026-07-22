d9194da66c37f67508940907519837bea815781e
marygracepandoro1907-afk <marygracepandoro1907-afk>
2026-07-22 15:28:57 +0800

    commit

--- src/main/java/fr/quentincillierre/hangman/application/MainApp.java
+++ src/main/java/fr/quentincillierre/hangman/application/MainApp.java
@@ -7,14 +7,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
-    
+
     private static Stage primaryStage;
 
     @Override
     public void start(Stage stage) throws Exception {
         primaryStage = stage;
         primaryStage.setTitle("Hangman Game");
-        
+
         // FIXED: Added absolute forward slash root indicator
         switchScene("/menu-view.fxml");
         primaryStage.show();
@@ -27,10 +27,10 @@ public class MainApp extends Application {
             if (fxmlUrl == null) {
                 throw new IllegalStateException("Cannot find layout asset file at path: " + fxmlPath);
             }
-            
+
             FXMLLoader loader = new FXMLLoader(fxmlUrl);
             Parent root = loader.load();
-            
+
             Scene scene = primaryStage.getScene();
             if (scene == null) {
                 scene = new Scene(root);
@@ -38,12 +38,12 @@ public class MainApp extends Application {
             } else {
                 scene.setRoot(root);
             }
-            
+
             // Re-apply styles if style.css exists
             if (MainApp.class.getResource("/style.css") != null) {
                 scene.getStylesheets().add(MainApp.class.getResource("/style.css").toExternalForm());
             }
-            
+
         } catch (Exception e) {
             System.err.println("Error changing scenes to " + fxmlPath + ": " + e.getMessage());
             e.printStackTrace();
